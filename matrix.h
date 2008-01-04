22d0534671b3609c072a9fbd905b8733457cf44f
pere.negre <pere.negre@c47dfba6-c041-0410-b03c-8bffbce334e4>
2008-01-04 23:15:11 +0000

    * When connected, one led stays on in the wiimote (to indicate that the connection is alive) * Added GPLv2 license in the source code files

--- matrix.h
+++ matrix.h
@@ -1,3 +1,23 @@
+/* Copyright (C) 2008 Pere Negre
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,10 +27,6 @@ typedef struct {
 	int h,v;
 } matrix_t;
 
-typedef struct {
-	float *d;
-	int h;
-} vector_t;
 
 extern matrix_t *   matrixNew(int h, int v);
 extern void         matrixFree(matrix_t *m);
--- wii.c
+++ wii.c
@@ -1,3 +1,26 @@
+/* Copyright (C) 2007 L. Donnie Smith <cwiid@abstrakraft.org>
+ * 
+ *  (modified by Pere Negre)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -62,10 +85,11 @@ int wii_connect(char *mac)
 		return 0;
         }
 
+	set_led_state(wiimote,1);
+
         toggle_bit(rpt_mode, CWIID_RPT_IR);
 	toggle_bit(rpt_mode, CWIID_RPT_BTN);
         set_rpt_mode(wiimote, rpt_mode);
-
         cwiid_enable(wiimote, CWIID_FLAG_MESG_IFC);
 	return 1;
 }
@@ -75,6 +99,8 @@ int wii_connect(char *mac)
 
 void wii_disconnect()
 {
+	set_led_state(wiimote,0);
+
 	if (cwiid_disconnect(wiimote)) {
                 fprintf(stderr, "Error on wiimote disconnect\n");
         }
