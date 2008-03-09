a509368833bd8e1bdb550049698be12fb6c91634
vanhtu1987 <vanhtu1987@c47dfba6-c041-0410-b03c-8bffbce334e4>
2008-03-09 18:07:12 +0000

    In the middle of i18n-ization.

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+#autoreconf -v --install || exit 1
+#intltoolize --copy --force --automake || exit 1
+#glib-gettextize --copy --force || exit 1
+
+gettextize --copy --force || exit 1
+autoreconf -v --install || exit 1
+
+cd $ORIGDIR || exit $?
+
+$srcdir/configure --enable-maintainer-mode "$@"
