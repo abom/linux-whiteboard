e8da1e9528793cec0e4cc89990bd14888a298386
vanhtu1987 <vanhtu1987@c47dfba6-c041-0410-b03c-8bffbce334e4>
2008-03-09 18:47:53 +0000

    Not bother calling gettextize anymore, it's annoying.

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,6 @@ cd $srcdir
 #intltoolize --copy --force --automake || exit 1
 #glib-gettextize --copy --force || exit 1
 
-gettextize --copy --force || exit 1
 autoreconf -v --install || exit 1
 
 cd $ORIGDIR || exit $?
