b921d840181418d6d62fc03ddae23c34a3187712
vanhtu1987 <vanhtu1987@c47dfba6-c041-0410-b03c-8bffbce334e4>
2008-03-09 18:54:09 +0000

    ...

--- branches/cpp/autogen.sh
+++ branches/cpp/autogen.sh
@@ -10,6 +10,7 @@ cd $srcdir
 #intltoolize --copy --force --automake || exit 1
 #glib-gettextize --copy --force || exit 1
 
+gettextize --copy --force || exit 1
 autoreconf -v --install || exit 1
 
 cd $ORIGDIR || exit $?
